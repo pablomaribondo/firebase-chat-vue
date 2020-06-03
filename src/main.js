2df8c8f9f2f1ff533df70eb93d5065c079387cb4
Pablo Maribondo <pablomgvieira@gmail.com>
2020-06-03 20:38:39 -0300

    Commit inicial

--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
 import Vue from 'vue';
+import firebase from 'firebase/app';
+import 'firebase/firestore';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 Vue.config.productionTip = false;
+
+const firebaseConfig = {
+// firebase config
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const db = firebase.firestore();
+Vue.prototype.$db = db;
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+<h3>Please login with your google account to continue</h3>
+
+<button @click="login">Login with google</button>
+  </div>
+</template>
+
+<script>
+import * as firebase from 'firebase/app';
+import 'firebase/auth';
+
+export default {
+  name: 'Login',
+
+  methods: {
+    async login() {
+      const provider = new firebase.auth.GoogleAuthProvider();
+
+      try {
+        const result = await firebase.auth().signInWithPopup(provider);
+        const token = result.credential.accessToken;
+        const { user } = result;
+
+        this.$router.push('/');
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
